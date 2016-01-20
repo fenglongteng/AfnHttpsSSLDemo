422be203b9adf3917f413f43a464757c58189770
jys <jys509@126.com>
2016-01-20 14:34:38 +0800

    Initial Commit

---
+++ AfnHttpsSSLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AfnHttpsSSLDemo
+//
+//  Created by Apple on 16/1/20.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
